7bd89ef495513a548994d715d292883049824da2
saraoriola <saraoriola@outlook.com>
2023-09-15 22:48:09 +0200

    Fonts y colors en variales

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import React from 'react';
+import './App.scss'; 
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+    <div className="app-container">
+    <h1 className="titulo">Título en Telegraf-UltraBold</h1>
+    <p className="texto-light">Este es un texto en Telegraf-UltraLight.</p>
+    <div className="colores-container">
+      <div className="color-box cream-bg">Cream</div>
+      <div className="color-box blue-bg">Blue</div>
+      <div className="color-box gray-bg">Gray</div>
+      <div className="color-box orange-bg">Orange</div>
+    </div>
+  </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
