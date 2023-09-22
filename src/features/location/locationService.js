9be6a24d60aac00d299041839f89053defd6a38c
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-22 18:25:45 +0200

    Refactoring of env variable

--- src/features/location/locationService.js
+++ src/features/location/locationService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : "https://eventum-back.vercel.app";
 
 const createLocation = async (formData) => {
     const res = await axios.post(`${API_URL}/locations/create`, formData);
