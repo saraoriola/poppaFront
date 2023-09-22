54785a97aa679186b7caaa1669f845a0298b0055
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-22 18:12:44 +0200

    Fix API_URL env variable

--- src/features/location/locationService.js
+++ src/features/location/locationService.js
@@ -1,28 +1,26 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const createLocation = async (formData) => {
-  const res = await axios.post(`${API_URL}/locations/create`, formData); 
-  return res.data;
+    const res = await axios.post(`${API_URL}/locations/create`, formData);
+    return res.data;
 };
 
 const deleteLocation = async (id) => {
-  const res = await axios.delete(`${API_URL}/locations/delete/${id}` )
-  return res.data;
-}
+    const res = await axios.delete(`${API_URL}/locations/delete/${id}`);
+    return res.data;
+};
 
 const getLocationById = async (id) => {
-  const res = await axios.get(`${API_URL}/locations/getbyid/${id}`)
-  return res.data;
-}
-
+    const res = await axios.get(`${API_URL}/locations/getbyid/${id}`);
+    return res.data;
+};
 
 const locationService = {
-  createLocation,
-  deleteLocation,
-  getLocationById
-
+    createLocation,
+    deleteLocation,
+    getLocationById,
 };
 
 export default locationService;
