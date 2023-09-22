2b06e65321a908776867d3dc6fd1d0e1da7898cf
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-22 18:36:38 +0200

    Use apiClient service

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : "https://eventum-back.vercel.app";
 
+console.log("API URL ->", API_URL);
+
 const apiClient = axios.create({
     baseURL: API_URL,
     withCredentials: true,
