e1346a6bf31bae917ad6f802925497198f316169
saraoriola <saraoriola@outlook.com>
2023-09-24 23:24:49 +0200

    modificada la ruta de dashboard

--- src/routes.jsx
+++ src/routes.jsx
@@ -37,7 +37,7 @@ const routes = createBrowserRouter([
         errorElement: <ErrorPage />,
     },
     {
-        path: "/dashboards/event/:id",
+        path: "dashboard",
         element: <Dashboard/>,
         errorElement: <ErrorPage />,
     },
