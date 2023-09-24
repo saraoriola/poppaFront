77c53ae857872f102f079a4cee58b27a241ca5f4
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-24 23:09:49 +0200

    Change axios by apiClient

--- src/features/location/locationService.js
+++ src/features/location/locationService.js
@@ -1,30 +1,24 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:3001";
+import apiClient from "../../api/apiClient"; //para las llamadas implementamos el apiClient
 
 const getLocationById = async (id) => {
-  const res = await axios.get(`${API_URL}/locations/getbyid/${id}`)
-  return res.data;
-}
+    const res = await axios.get(`${API_URL}/locations/getbyid/${id}`);
+    return res.data;
+};
 
 const createLocation = async (formData) => {
-  const res = await axios.post(`${API_URL}/locations/create`, formData); 
-  return res.data;
+    const res = await apiClient.post(`${API_URL}/locations/create`, formData);
+    return res.data;
 };
 
 const deleteLocation = async (id) => {
-  const res = await axios.delete(`${API_URL}/locations/delete/${id}` )
-  return res.data;
-}
-
-
+    const res = await axios.delete(`${API_URL}/locations/delete/${id}`);
+    return res.data;
+};
 
 const locationService = {
-  createLocation,
-  deleteLocation,
-  getLocationById
-
+    createLocation,
+    deleteLocation,
+    getLocationById,
 };
 
 export default locationService;
-
