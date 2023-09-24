4c8127901925375c841729897daf8735124be565
saraoriola <saraoriola@outlook.com>
2023-09-24 15:33:47 +0200

    Añadido HeaderEvent a falta de ver por que no importa bien la img

--- src/components/EventUser/Dashboard/HeaderEvent/HeaderEvent.jsx
+++ src/components/EventUser/Dashboard/HeaderEvent/HeaderEvent.jsx
@@ -1,23 +1,81 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getEventById } from "../../../../features/dashboards/dashboardsSlice";
+import { Box, Card, Text, VStack, Flex } from '@chakra-ui/react';
+
+const cardStyle = {
+  backgroundImage: 'url("../../../../assets/images/fotoevento.jpg)',
+  backgroundSize: 'cover',
+  height: '150px',
+  position: 'relative',
+};
+
+const overlayStyle = {
+  content: '""',
+  background: 'rgba(0, 67, 104, 0.8)',
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+};
+
+const descriptionBoxStyle = {
+  position: 'absolute',
+  top: '34%',
+  left: '0',
+  transform: 'translateY(-50%)',
+  color: 'white',
+  flex: '7',
+  maxWidth: '70%',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+};
 
 const HeaderEvent = () => {
   const { id } = useParams();
+  const { event } = useSelector(state => state.dashboards);
   const dispatch = useDispatch();
-console.log(id)
 
-useEffect(() => {
-  dispatch(getEventById(id));
-}, [id, dispatch]);
-
+  useEffect(() => {
+    dispatch(getEventById(id));
+  }, [id, dispatch]);
 
   return (
-<>
+    <>
+      <Box position="relative">
+        <Card style={cardStyle}>
+          <div style={overlayStyle}></div>
+        </Card>
 
-HOLA
-</>  )
+        <Box position="absolute" top="30%" right="5" transform="translateY(-50%)" color="white" flex="3">
+          <VStack align="center" p="1rem" spacing={0}>
+            <Text fontSize="xl" textAlign="center" className='p'>
+            {event.day}
+            </Text>
+            <Text fontSize="md" textAlign="center" className='h1'>
+            {event.month}
+            </Text>
+          </VStack>
+        </Box>
+
+        <Box style={descriptionBoxStyle}>
+          <VStack align="start" p="1rem" spacing={0}>
+            <Text fontSize="sm" fontWeight="bold" textAlign="left" className='h1'>
+              {event.title}
+            </Text>
+            <Text fontSize="xs" textAlign="left" className='p'>
+              {event.description}
+            </Text>
+            <Text fontSize="sm" textAlign="left" className='p'>
+              {event.speacker}
+            </Text>
+          </VStack>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
-export default HeaderEvent
+export default HeaderEvent;
--- src/features/dashboards/dashboardsSlice.js
+++ src/features/dashboards/dashboardsSlice.js
@@ -3,12 +3,29 @@ import dashboardsService from './dashboardsService';
 
 const initialState = {
   events: [],
+  event: {}
 };
 
 export const dashboardsSlice = createSlice({
   name: 'dashboards',
   initialState,
-  reducers: {},
+  reducers: {
+
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(getEventById.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getEventById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.event = action.payload;
+      })
+      .addCase(getEventById.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+    },
 });
 
 export const getEventById = createAsyncThunk('dashboards/getEventById', async (id) => {
