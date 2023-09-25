e2151d5fe5b96090c9652e342150ebc88b60bb73
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-25 12:50:38 +0200

    Bug fixing and refactoring

--- src/components/Header/Profile/Profile.jsx
+++ src/components/Header/Profile/Profile.jsx
@@ -1,17 +1,14 @@
 import { Avatar } from "@chakra-ui/react";
-import React from "react";
 import { useSelector } from "react-redux";
 
 const Profile = () => {
-  const userConnected = useSelector((state) => state.auth.userConnected);
+    const userConnected = useSelector((state) => state.auth.userConnected);
 
-  const { avatar, name } = userConnected;
-
-  return (
-    <>
-      <Avatar name={name} src={avatar} size={"sm"} />
-    </>
-  );
+    return (
+        <>
+            <Avatar name={userConnected?.name} src={userConnected?.avatar} size={"sm"} />
+        </>
+    );
 };
 
 export default Profile;
