1c6dfc6064a3cedb9930e4b4530ac66b03e7211f
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-25 11:06:56 +0200

    Added style in home still unfinished

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,29 +8,29 @@ import GetAllEvents from "../Event/GetAllEvents/GetAllEvents";
 import PrintEvents from "../Event/PrintEvents/PrintEvents";
 
 const Home = () => {
-  const [searchResults, setSearchResults] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
 
-  // Función para manejar los resultados de la búsqueda
-  const handleSearch = (results) => {
-    setSearchResults(results);
-  };
+    // Función para manejar los resultados de la búsqueda
+    const handleSearch = (results) => {
+        setSearchResults(results);
+    };
 
-  return (
-    <Box className="home-container">
-      <Box className="father-header-container">
-        <HeaderRender onSearch={handleSearch} />
-      </Box>
-      <Box className="home-title">Próximos eventos</Box>
-      <Box className="father-filter-container">
-        <FilterEvents />
-      </Box>
-      <Box className="father-cards-container">
-        <GetAllEvents />
-        {/* FIXME: Esto ha dejado de ir :c Hay bugs*/}
-        <PrintEvents results={searchResults} />
-      </Box>
-    </Box>
-  );
+    return (
+        <Box className="home-container">
+            <Box className="father-header-container">
+                <HeaderRender onSearch={handleSearch} />
+            </Box>
+            <Box className="home-title">Próximos eventos</Box>
+            <Box className="father-filter-container">
+                <FilterEvents />
+            </Box>
+            <Box className="father-cards-container">
+                <GetAllEvents />
+                {/* FIXME: Esto ha dejado de ir :c Hay bugs*/}
+                <PrintEvents results={searchResults} />
+            </Box>
+        </Box>
+    );
 };
 
 export default Home;
