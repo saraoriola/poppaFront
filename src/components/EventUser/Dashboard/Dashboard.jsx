31dbdff8436ab19299f89d7440dae6703c715d9a
saraoriola <saraoriola@outlook.com>
2023-09-25 22:47:16 +0200

    Dahs start

--- src/components/EventUser/Dashboard/Dashboard.jsx
+++ src/components/EventUser/Dashboard/Dashboard.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import HeaderEvent from "./HeaderEvent/HeaderEvent";
 import Attendees from "./Attendees/Attendees";
+import HeaderDash from "./HeaderDash";
 
 function Dashboard() {
   const { id } = useParams();
 
   return (
     <div>
-      <h1>Detalles del evento {id}</h1>
-     
+<HeaderDash/> 
       <HeaderEvent/>
       <Attendees/>
     </div>
