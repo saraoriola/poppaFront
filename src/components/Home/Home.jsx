af32d799c2ca3875a191ffdc18c97f504901f8a5
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-25 11:06:56 +0200

    Added style in home still unfinished

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,28 +2,30 @@ import { useSelector } from "react-redux";
 import Logout from "../User/Auth/Logout";
 import Login from "../User/Auth/Login";
 import { Link } from "react-router-dom";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Grid, Stack } from "@chakra-ui/react";
 import EventCreate from "../Event/CreateEvent/EventCreate";
 
 const Home = () => {
     const user = useSelector((state) => state.auth.user);
     return (
-        <Box>
-            {!user && (
-                <Link to="register">
-                    <Button ml="60%" mt="8%" colorScheme="blue">
-                        Registrarse
-                    </Button>
-                </Link>
-            )}
-            {user && (
-                <>
-                    <Logout />
-                    <EventCreate />
-                </>
-            )}
-            {!user && <Login />}
-        </Box>
+        <Grid bgColor="#e6dfcf">
+            <Box>
+                {!user && (
+                    <Link to="register">
+                        <Button ml="60%" mt="8%" colorScheme="blue">
+                            Registrarse
+                        </Button>
+                    </Link>
+                )}
+                {user && (
+                    <>
+                        <Logout />
+                        <EventCreate />
+                    </>
+                )}
+                {!user && <Login />}
+            </Box>
+        </Grid>
     );
 };
 
