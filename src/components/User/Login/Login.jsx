cddf91bd832a6d7094aa7f5d2b825f1d22366f14
saraoriola <saraoriola@outlook.com>
2023-09-25 19:43:42 +0200

    Login ok

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../../../features/auth/authSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   Box,
   Card,
@@ -12,10 +12,14 @@ import {
   Text,
   VStack,
   useToast,
+  Image,
+  Checkbox,
+  Spacer,
 } from "@chakra-ui/react";
 
-import "./Login.scss"
-import HeaderRender from "../../Header/HeaderRender/HeaderRender";
+import "./Login.scss";
+import logo from "../../../assets/images/logomarinaazul.png"
+
 const Login = () => {
   const [formData, setFormData] = useState({
     email: "",
@@ -59,58 +63,89 @@ const Login = () => {
   };
 
   return (
-    <>
-    <HeaderRender/>
-    <Box ml={4} mr={4}>
+    <Box
+      backgroundColor="#f8f8f8"
+      height="100vh"
+      width="100%"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+    >
+      <form onSubmit={onSubmit}>
+        <VStack align="center">
+          <Text as="b" fontSize="xl" className="title" justifyContent="center">
+            Te damos 
+          </Text>
+          <Text as="b" fontSize="xl" className="title" justifyContent="center" mt={-4}>
+            la bienvenida a
+          </Text>
+          <Text as="b" fontSize="4xl" className="title" mt={-2}>
+            Eventum
+          </Text>
+          <Text as="b" fontSize="lg" className="title" justifyContent="center" mt={5}>
+            la plataforma de eventos de
+          </Text>
+          <Image src={logo} mt={3}/>
+          <Box m={10}>
+  <FormControl id="email" isRequired  className="form-container">
+    <FormLabel fontSize="xs" mb={1}>
+      Correo electrónico
+    </FormLabel>
+    <Input
+      width="312px" 
+      height="32px"
+      type="email"
+      name="email"
+      placeholder="email@edem.es"
+      value={email}
+      onChange={onChange}
+      borderColor="#E6E5E5"
+      backgroundColor="#f8f8f8"
+      borderRadius={4}
+      className="input-placeholder"
+      fontSize="xs"
+    />
+  </FormControl>
+  <FormControl id="password" isRequired mt={4}>
+    <FormLabel className="input input-placeholder" fontSize="xs" mb={1}>
+      Contraseña
+    </FormLabel>
+    <Input
+      width="312px" 
+      height="32px"
+      type="password"
+      name="password"
+      placeholder="password"
+      value={password}
+      onChange={onChange}
+      borderColor="#E6E5E5"
+      backgroundColor="#f8f8f8"
+      borderRadius={4}
+      className="input input-placeholder"
+      fontSize="xs"
+    />
+  </FormControl>
+</Box>
+<Box display="flex" justifyContent="space-between" alignItems="center" mt={2}>
+  <Checkbox mr="60px" size="sm" colorScheme="teal">Recuérdame</Checkbox>
+  <Text fontSize="xs" color="#cb7862" cursor="pointer">
+    He olvidado mi contraseña
+  </Text>
+</Box>
 
-        <form onSubmit={onSubmit}>
-          <VStack spacing="4" align="center">
-            <Text as="b" fontSize="1.2em" mb="3%" className="title" >
-              Bienvenid@
-            </Text>
-            <FormControl id="email" isRequired >
-              <FormLabel className="imput  input-placeholder" fontSize="xs" mb={1}>Email</FormLabel>
-              <Input
-                height="32px" 
-                type="email"
-                name="email"
-                placeholder="email@edem.es"
-                value={email}
-                onChange={onChange}
-                borderColor="#cb7862"
-                backgroundColor="#f8f8f8"
-                borderRadius={4}
-                className="imput  input-placeholder"
-                fontSize="xs"
-              />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel className="imput  input-placeholder" fontSize="xs" mb={1}>Contraseña</FormLabel>
-              <Input
 
-  height="32px" 
-  type="password"
-  name="password"
-  placeholder="password"
-  value={password}
-  onChange={onChange}
-  borderColor="#cb7862"
-  backgroundColor="#f8f8f8"
-  borderRadius={4}
-  className="imput  input-placeholder"
-  fontSize="xs"
-/>
+          <Button width="312px" height="32px" colorScheme="blue" type="submit" bg="#cb7862" borderRadius={8}>
+            Acceder
+          </Button>
+        </VStack>
+      </form>
+      <Box mt={4} textAlign="center" display="flex" alignItems="center" justifyContent="center">
+  <Text fontSize="sm">¿No tienes cuenta? <Link to="/register" style={{ color: "#cb7862" }}>Regístrate</Link></Text>
 
-            </FormControl>
-            <Button mt="8%" colorScheme="blue" type="submit" bg="#cb7862" borderRadius={8} width="148px">
-              Iniciar sesión
-            </Button>
-          </VStack>
-        </form>
+</Box>
+
+
     </Box>
-    
-    
-    </>
   );
 };
 
