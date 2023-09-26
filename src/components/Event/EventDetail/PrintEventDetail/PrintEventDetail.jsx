b8d439ac5f7bbdba5ffb98726091b748b46b6053
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-27 00:15:05 +0200

    Added changes in Printeventdetail

--- src/components/Event/EventDetail/PrintEventDetail/PrintEventDetail.jsx
+++ src/components/Event/EventDetail/PrintEventDetail/PrintEventDetail.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
-import { Spinner } from "@chakra-ui/spinner";
-import { Card, CardBody, CardFooter, CardHeader } from "@chakra-ui/card";
-import { Image } from "@chakra-ui/image";
-import { Box, Text } from "@chakra-ui/layout";
 import { getEventById } from "../../../../features/event/eventSlice";
 import eventImg from "../../../../assets/images/desafio.jpg";
 import EventDetailButtons from "../EventDetailButtons/EventDetailButtons";
-import { AiFillCalendar, AiOutlineClockCircle } from "react-icons/ai";
+import { BsCalendar3 } from "react-icons/bs";
+import { GoLocation } from "react-icons/go";
+import { BiTimeFive } from "react-icons/bi";
+import { Card, Box, Text, HStack, Image, Spinner } from "@chakra-ui/react";
 
 const PrintEventDetail = () => {
     const { id } = useParams();
@@ -33,55 +32,41 @@ const PrintEventDetail = () => {
 
     return (
         <>
-            <Box></Box>
+            <Card m="auto" mt="12px" maxW="22.5rem" bg="#e6dfcf" boxShadow="0">
+                <Image src={eventImg} alt="Banner" w="312px" h="140px" ml="24px" mt="20px" borderRadius="4px" objectFit="cover" />
+                <Text pl="24px" mt="5px" fontFamily="Nocturne-Black" color="#004368" fontSize="16px">
+                    {event.title}
+                </Text>
+                <BsCalendar3 size={15} color="#cb7862" />
+                <Text fontFamily="Telegraf-UltraLight" color="#847c7b">
+                    {event.dateTime}
+                </Text>
+                <HStack>
+                    <BiTimeFive size={20} color="#cb7862" />
+                    <Box>{event.duration_min}</Box>
+                </HStack>
+                <HStack>
+                    <GoLocation size={15} color="#cb7862" />
+                    <Box>EDEM</Box>
+                </HStack>
+                <GoLocation size={15} color="#cb7862" />
+                <Box>{event.location_id}</Box>
+            </Card>
+
             <Box>
-                <Card m="auto" mt="12px" maxW="22.5rem" bg="#e6dfcf" boxShadow="0">
-                    <Image src={eventImg} alt="Banner" w="312px" h="140px" ml="24px" mt="20px" borderRadius="4px" objectFit="cover" />
+                <EventDetailButtons />
+            </Box>
 
-                    <Text pl="24px" mt="5px" fontFamily="Nocturne-Black" color="#004368" fontSize="16px">
-                        {event.title}
-                    </Text>
+            <Box borderRadius="15px" m="0px 25px 12px 25px" padding="10px" bg="#f2f2f2" textAlign="justify" color="#847c7b">
+                <Text>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae corporis, sequi quam hic facere esse aperiam praesentium accusamus quis aut, autem consequuntur quos est
+                    exercitationem labore non suscipit omnis porro.
+                </Text>
+            </Box>
 
-                    <CardFooter display="column" p="0px 10px 10px" fontFamily="Telegraf-UltraLight" color="#847c7b">
-                        <Box display="flex" justifyContent="space-between">
-                            <Box display="flex" alignItems="center" gap="5px">
-                                <AiFillCalendar color="#cb7862" />
-                                <Box>{event.dateTime}</Box>
-                            </Box>
-                            <Box display="flex" alignItems="center" gap="5px">
-                                <AiOutlineClockCircle color="#cb7862" />
-                                <Box>{event.duration_min}</Box>
-                            </Box>
-                        </Box>
-                        <Box display="flex" justifyContent="space-between">
-                            <Box display="flex" alignItems="center" gap="5px">
-                                <AiFillCalendar color="#cb7862" />
-                                <Box>EDEM</Box>
-                            </Box>
-                            <Box display="flex" alignItems="center" gap="5px">
-                                <AiOutlineClockCircle color="#cb7862" />
-                                <Box>{event.location_id}</Box>
-                            </Box>
-                        </Box>
-                        <Box display="flex" justifyContent="space-between"></Box>
-                    </CardFooter>
-                </Card>
-
-                <Box>
-                    <EventDetailButtons />
-                </Box>
-
-                <Box borderRadius="15px" m="0px 25px 12px 25px" padding="10px" bg="#f2f2f2" textAlign="justify" color="#847c7b">
-                    <Text>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae corporis, sequi quam hic facere esse aperiam praesentium accusamus quis aut, autem consequuntur quos est
-                        exercitationem labore non suscipit omnis porro.
-                    </Text>
-                </Box>
-
-                <Box m="10px 25px 10px 25px">
-                    <Text>Categorias</Text>
-                    <Text fontFamily="Telegraf-UltraLight">Implementar etiquetas. Lo pongo aquí porque se supone que son del evento.</Text>
-                </Box>
+            <Box m="10px 25px 10px 25px">
+                <Text>Categorias</Text>
+                <Text fontFamily="Telegraf-UltraLight">Implementar etiquetas. Lo pongo aquí porque se supone que son del evento.</Text>
             </Box>
         </>
     );
