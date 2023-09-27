b49e1ea9d6480102d7dcd8e2619c54b3cee6e38f
saraoriola <saraoriola@outlook.com>
2023-09-27 16:04:32 +0200

    Añadidos links a footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,13 @@
-import { Box } from "@chakra-ui/react";
 import React from "react";
+import { Box} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <Box
-      height="40px"
-      display="flex"
-      justifyContent="space-around"
-      alignItems="center"
-      bg="#847c7b"
-      color="#f2f2f2"
-      fontFamily="PPTelegraf-UltraLight"
-      fontSize="xs"
-      textDecor="underline"
-    >
-      <Link to="https://marinadeempresas.es/politica-de-cookies/">
-        Política de Cookies
-      </Link>
+    <Box height="40px" display="flex" justifyContent="space-around" alignItems="center" bg="#847c7b" color="#f2f2f2" fontFamily="PPTelegraf-UltraLight" fontSize="xs" textDecor="underline">
+      <Link to="https://marinadeempresas.es/politica-de-cookies/">Política de Cookies</Link>
       <Link to="https://marinadeempresas.es/aviso-legal/">Aviso Legal</Link>
-      <Link to="https://marinadeempresas.es/politica-de-privacidad/">
-        Política de Privacidad
-      </Link>
+      <Link to="https://marinadeempresas.es/politica-de-privacidad/">Política de Privacidad</Link>
     </Box>
   );
 };
