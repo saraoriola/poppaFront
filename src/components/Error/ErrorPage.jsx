a72b5287990ce72e6b7a91881bae415c756b4d5a
saraoriola <saraoriola@outlook.com>
2023-09-27 15:15:10 +0200

    ErrorPage clean code

--- src/components/Error/ErrorPage.jsx
+++ src/components/Error/ErrorPage.jsx
@@ -1,34 +1,35 @@
-import { Box, Image, Text, Center, Button, Flex } from "@chakra-ui/react";
-import { Link, useRouteError } from "react-router-dom";
-import pageNotFoundImg from "../../assets/images/404.svg";
+import React from 'react';
+import { Box, Image, Text, Center, Button, Flex } from '@chakra-ui/react';
+import { Link, useRouteError } from 'react-router-dom';
+import pageNotFoundImg from '../../assets/images/404.svg';
 
 const ErrorPage = () => {
-    const error = useRouteError();
+  const error = useRouteError();
 
-    return (
-        <Flex pb="18%" justifyContent="center" alignItems="center">
-            <Box>
-                <Box>
-                    <Text ml="90px" mt="100px" fontFamily="Nocturne-Black" fontWeight="900" fontSize="36px" color="#004368">
-                        ¡Ooops!
-                    </Text>
-                    <Text mt="24px" mb="50px" fontFamily="Nocturne-Regular" fontWeight="400" fontSize="20px" color="#004368" textAlign="center">
-                        ¡Algo ha ocurrido! <br />
-                        No sabemos qué ha podido <br /> pasar pero lo arreglaremos...
-                    </Text>
-                </Box>
-                <Image w="312px" h="169px" ml="10px" src={pageNotFoundImg} alt="Imagen" />
-                <Text mt="50px" mb="40px" fontFamily="Nocturne-Regular" fontWeight="400" fontSize="20px" color="#004368" textAlign="center">
-                    No pasa nada, te volvemos <br />a indicar el camino a casa...
-                </Text>
-                <Link to="/">
-                    <Button w="170px" h="48px" mt="50px" ml="80px" bg="#cb7862" color="white" fontSize="19px">
-                        Volver a inicio
-                    </Button>
-                </Link>
-            </Box>
-        </Flex>
-    );
+  return (
+    <Flex pb="18%" justifyContent="center" alignItems="center">
+      <Box>
+        <Box>
+          <Text ml="90px" mt="100px" fontFamily="Nocturne-Black" fontWeight="900" fontSize="36px" color="#004368">
+            ¡Ooops!
+          </Text>
+          <Text mt="24px" mb="50px" fontFamily="Nocturne-Regular" fontWeight="400" fontSize="20px" color="#004368" textAlign="center">
+            ¡Algo ha ocurrido! <br />
+            No sabemos qué ha podido <br /> pasar pero lo arreglaremos...
+          </Text>
+        </Box>
+        <Image w="312px" h="169px" ml="10px" src={pageNotFoundImg} alt="Imagen" />
+        <Text mt="50px" mb="40px" fontFamily="Nocturne-Regular" fontWeight="400" fontSize="20px" color="#004368" textAlign="center">
+          No pasa nada, te volvemos <br />a indicar el camino a casa...
+        </Text>
+        <Link to="/">
+          <Button w="170px" h="48px" mt="50px" ml="80px" bg="#cb7862" color="white" fontSize="19px">
+            Volver a inicio
+          </Button>
+        </Link>
+      </Box>
+    </Flex>
+  );
 };
 
 export default ErrorPage;
