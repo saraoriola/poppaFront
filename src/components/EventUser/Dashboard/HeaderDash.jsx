c566aaccf88457e675819970c456aa49513cd20b
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-27 12:23:36 +0200

    Fixed routes in dashboard and profile

--- src/components/EventUser/Dashboard/HeaderDash.jsx
+++ src/components/EventUser/Dashboard/HeaderDash.jsx
@@ -1,34 +1,20 @@
-import React from 'react';
-import { Box, Image, Link as RouterLink } from '@chakra-ui/react';
-import { AiOutlineArrowLeft } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Box, Image, Link as RouterLink } from "@chakra-ui/react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 import logo from "../../../assets/images/Logotipo.png";
 
 const HeaderDash = () => {
-  return (
-    <Box
-      width="100%"
-      height="44px"
-      backgroundColor="#004368"
-      position="sticky"
-      top="0"
-      display="flex"
-      alignItems="center"
-      paddingX="1rem"
-    >
-      <RouterLink as={Link} to="/login" >
-        <AiOutlineArrowLeft
-          style={{ cursor: 'pointer' }}
-          size={24}
-          color="white"
-        />
-      </RouterLink>
+    return (
+        <Box width="100%" height="44px" backgroundColor="#004368" position="sticky" top="0" display="flex" alignItems="center" paddingX="1rem">
+            <RouterLink as={Link} to="/approved/accepted">
+                <AiOutlineArrowLeft style={{ cursor: "pointer" }} size={24} color="white" />
+            </RouterLink>
 
-
-      <Image src={logo} alt="Logo"  style={{ margin: '0 auto' }}  />
-    </Box>
-  );
+            <Image src={logo} alt="Logo" style={{ margin: "0 auto" }} />
+        </Box>
+    );
 };
 
 export default HeaderDash;
