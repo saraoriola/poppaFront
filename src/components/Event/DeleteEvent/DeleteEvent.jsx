1e2622b5986f03498c08fa920d01ee00e872b834
saraoriola <saraoriola@outlook.com>
2023-09-27 15:23:18 +0200

    Delete event added

--- src/components/Event/DeleteEvent/DeleteEvent.jsx
+++ src/components/Event/DeleteEvent/DeleteEvent.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Text } from "@chakra-ui/react";
 
-const DeleteEvent = () => {
-    return <div>DeleteEvent</div>;
+const DeleteEvent = ({ isOpen, onClose, onDelete }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Confirmar eliminación</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>¿Estás seguro de que deseas eliminar este evento?</Text>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="red" onClick={onDelete}>
+            Eliminar
+          </Button>
+          <Button onClick={onClose}>Cancelar</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 };
 
 export default DeleteEvent;
