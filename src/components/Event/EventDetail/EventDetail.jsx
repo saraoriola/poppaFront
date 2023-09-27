24b6d6dd0c8dee8efcc61c5e33616a2f0edeea44
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-09-27 08:31:19 +0200

    solucionado lo de la sintaxis

--- src/components/Event/EventDetail/EventDetail.jsx
+++ src/components/Event/EventDetail/EventDetail.jsx
@@ -4,22 +4,23 @@ import PrintEventDetail from "./PrintEventDetail/PrintEventDetail";
 import HeaderRender from "../../Header/HeaderRender/HeaderRender";
 
 const EventDetail = () => {
-    return (
-        <Box bg="#e6dfcf">
-            <Box>
-                <HeaderRender />
-            </Box>
-            <Box>
-                <PrintEventDetail />
-                <QrGenerator eventId={5} />
-            </Box>
+  return (
+    <Box>
+      <Box bg="#e6dfcf">
+        <Box>
+          <HeaderRender />
         </Box>
         <Box>
           <PrintEventDetail />
-          {/* <QrGenerator eventId={5} /> */}
+          <QrGenerator eventId={5} />
         </Box>
       </Box>
-    );
+      <Box>
+        <PrintEventDetail />
+        {/* <QrGenerator eventId={5} /> */}
+      </Box>
+    </Box>
+  );
 };
 
 export default EventDetail;
