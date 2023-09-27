c2aab5fc2aa068f177d1532432c4da57cd93e8d3
saraoriola <saraoriola@outlook.com>
2023-09-27 15:41:57 +0200

    FilterEvents clean code

--- src/components/Event/FilterEvents/FilterEvents.jsx
+++ src/components/Event/FilterEvents/FilterEvents.jsx
@@ -43,88 +43,27 @@ const FilterEvents = () => {
             <Box overflowX="auto" whiteSpace="nowrap" mb={5}>
                 <Box className="button-scroll-container" display="inline-block">
                     <Button
-                        width="auto"
-                        height="auto"
-                        borderRadius="20px"
-                        color="#cb7862"
-                        fontFamily="PPTelegraf-UltraLight"
-                        fontSize={14}
-                        mr={2}
-                        onClick={() => handleFilterChange("Todos")}
-                        _hover={{ bg: "#cb7862", color: "white" }}
-                        _active={{ bg: "#cb7862", color: "white" }}
-                        style={{ background: "#cb7862", color: "white" }}
-                    >
-                        <Text as="span" whiteSpace="normal" paddingY="6px" paddingX="6px">
-                            Todos
-                        </Text>
+                        width="auto" height="auto" borderRadius="20px" color="#cb7862" fontFamily="PPTelegraf-UltraLight" fontSize={14} mr={2} onClick={() => handleFilterChange("Todos")}  _hover={{ bg: "#cb7862", color: "white" }}  _active={{ bg: "#cb7862", color: "white" }} style={{ background: "#cb7862", color: "white" }}>
+                        <Text as="span" whiteSpace="normal" paddingY="6px" paddingX="6px"> Todos</Text>
                     </Button>
-                    <Button
-                        width="auto"
-                        height="auto"
-                        borderRadius="20px"
-                        color="#cb7862"
-                        fontFamily="PPTelegraf-UltraLight"
-                        fontSize={14}
-                        mr={2}
-                        onClick={() => handleFilterChange("Inscrito")}
-                        _hover={{ bg: "#cb7862", color: "white" }}
-                        _active={{ bg: "#cb7862", color: "white" }}
-                        style={{ background: "#f2f2f2", color: "#cb7862" }}
-                    >
+                    <Button width="auto" height="auto" borderRadius="20px" color="#cb7862"  fontFamily="PPTelegraf-UltraLight"  fontSize={14}  mr={2}  onClick={() => handleFilterChange("Inscrito")}  _hover={{ bg: "#cb7862", color: "white" }} _active={{ bg: "#cb7862", color: "white" }} style={{ background: "#f2f2f2", color: "#cb7862" }}>
                         <Text as="span" whiteSpace="normal" paddingY="6px" paddingX="6px">
                             Inscrito
                         </Text>
                     </Button>
-                    <Button
-                        width="auto"
-                        height="auto"
-                        borderRadius="20px"
-                        color="#cb7862"
-                        fontFamily="PPTelegraf-UltraLight"
-                        fontSize={14}
-                        mr={2}
-                        onClick={() => handleFilterChange("Pendiente")}
-                        _hover={{ bg: "#cb7862", color: "white" }}
-                        _active={{ bg: "#cb7862", color: "white" }}
-                        style={{ background: "#f2f2f2", color: "#cb7862" }}
-                    >
+                    <Button  width="auto"  height="auto" borderRadius="20px"   color="#cb7862"   fontFamily="PPTelegraf-UltraLight"  fontSize={14}mr={2} onClick={() => handleFilterChange("Pendiente")}  _hover={{ bg: "#cb7862", color: "white" }}  _active={{ bg: "#cb7862", color: "white" }} style={{ background: "#f2f2f2", color: "#cb7862" }}>
                         <Text as="span" whiteSpace="normal" paddingY="6px" paddingX="6px">
                             Pendientes de aprobación
                         </Text>
                     </Button>
                     {userRole?.type === "admin" && (
-                        <Button
-                            width="auto"
-                            height="auto"
-                            borderRadius="20px"
-                            color="#cb7862"
-                            fontFamily="PPTelegraf-UltraLight"
-                            fontSize={14}
-                            mr={2}
-                            onClick={() => handleFilterChange("Organizo")}
-                            _hover={{ bg: "#cb7862", color: "white" }}
-                            _active={{ bg: "#cb7862", color: "white" }}
-                            style={{ background: "#f2f2f2", color: "#cb7862" }}
-                        >
+                        <Button width="auto" height="auto" borderRadius="20px"color="#cb7862" fontFamily="PPTelegraf-UltraLight" fontSize={14}   mr={2} onClick={() => handleFilterChange("Organizo")} _hover={{ bg: "#cb7862", color: "white" }} _active={{ bg: "#cb7862", color: "white" }} style={{ background: "#f2f2f2", color: "#cb7862" }}>
                             <Text as="span" whiteSpace="normal" paddingY="6px" paddingX="6px">
                                 Organizo
                             </Text>
                         </Button>
                     )}
-                    <Button
-                        width="auto"
-                        height="auto"
-                        borderRadius="20px"
-                        color="#cb7862"
-                        fontFamily="PPTelegraf-UltraLight"
-                        fontSize={14}
-                        mr={2}
-                        onClick={() => handleFilterChange("Finalizado")}
-                        _hover={{ bg: "#cb7862", color: "white" }}
-                        _active={{ bg: "#cb7862", color: "white" }}
-                        style={{ background: "#f2f2f2", color: "#cb7862" }}
-                    >
+                    <Button width="auto"height="auto" borderRadius="20px" color="#cb7862" fontFamily="PPTelegraf-UltraLight"fontSize={14}mr={2} onClick={() => handleFilterChange("Finalizado")} _hover={{ bg: "#cb7862", color: "white" }} _active={{ bg: "#cb7862", color: "white" }}style={{ background: "#f2f2f2", color: "#cb7862" }}>
                         <Text as="span" whiteSpace="normal" paddingY="6px" paddingX="6px">
                             Finalizado
                         </Text>
