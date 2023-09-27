5592d1c2268f2bd0deac4488a4b8fc279f4bd867
saraoriola <saraoriola@outlook.com>
2023-09-27 13:01:40 +0200

    flacha back funcionando

--- src/components/Header/HeaderRender/HeaderRender.jsx
+++ src/components/Header/HeaderRender/HeaderRender.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import Profile from "../Profile/Profile";
 import { Box, Image } from "@chakra-ui/react";
-import logo from "../../../assets/images/Logotipo.png";
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/images/Logotipo.png";
 
 const HeaderRender = () => {
+  const goBack = () => {
+    window.history.back();
+  };
+  
+
   return (
     <Box
-      display="flex"
+      d="flex"
       alignItems="center"
       justifyContent="space-between"
-      width="100%"
-      height="44px"
-      backgroundColor="#004368"
+      w="100%"
+      h="44px"
+      bg="#004368"
     >
-      <Box ml="15px">
-        <AiOutlineArrowLeft
-          style={{ cursor: "pointer" }}
-          size="28"
-          color="white"
-        />
+      <Box ml="15px" onClick={goBack} cursor="pointer">
+        <AiOutlineArrowLeft to="javascript:history.back()" size="28" color="white" />
       </Box>
       <Box>
         <Image src={logo} alt="Logo" />
