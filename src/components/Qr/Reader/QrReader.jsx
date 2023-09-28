0283815c0737dcc3f87dc73fdb180325a23f80df
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-28 02:13:10 +0200

    Added functionality in QRreader

--- src/components/Qr/Reader/QrReader.jsx
+++ src/components/Qr/Reader/QrReader.jsx
@@ -1,21 +1,27 @@
 import { Radio, RadioGroup, Stack, Text, Box } from "@chakra-ui/react";
 import useQrReader from "../../../hooks/useQrReader";
 import "./QrReader.scss";
+import { useState } from "react"; // Importa useState desde React
 
 const QrReader = () => {
     const [scanResult, setScanResult, isCheckIn, setIsCheckIn] = useQrReader();
+    const [showMessage, setShowMessage] = useState(false); // Define el estado showMessage
 
     const resetScanner = (isCheckIn) => {
         setIsCheckIn(isCheckIn);
         setScanResult(null);
+        setShowMessage(true); // Mostrar el mensaje cuando se cambia isCheckIn
+        setTimeout(() => {
+            setShowMessage(false); // Ocultar el mensaje después de 2 segundos
+        }, 2000);
     };
 
     return (
-        <Box pt="200px" pb="350px">
-            <RadioGroup defaultValue="1">
+        <Box>
+            <RadioGroup defaultValue="1" ml="20px" mt="50px" p="10px">
                 <Stack spacing={5} direction="row">
                     <Radio colorScheme="green" value="1" onChange={() => resetScanner(true)}>
-                        Check-in
+                        Escanear usuario
                     </Radio>
                     {/* <Radio colorScheme="red" value="2" onChange={() => resetScanner(false)}>
                         Check-out
@@ -23,9 +29,11 @@ const QrReader = () => {
                 </Stack>
             </RadioGroup>
             <div id="reader"></div>
-            {scanResult && (
+
+            {/* Mostrar el mensaje */}
+            {showMessage && (
                 <Text fontSize="20px" color={isCheckIn ? "green.200" : "tomato"}>
-                    {scanResult}
+                    {isCheckIn ? "Registrado" : "Error al registrar"}
                 </Text>
             )}
         </Box>
